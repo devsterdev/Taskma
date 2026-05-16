326b2696b0e63611eec47bf6458fb3b404a70107
Dev <sharmadevdeep9@gmail.com>
2026-05-17 00:53:59 +0530

    cors bug fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(passport.initialize());
 
 app.use(cors({
-  origin: true,
+   origin: process.env.FRONTEND_URL,
   credentials: true
 }))
 app.use(express.json({ limit: "16kb" }))
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
-const API_BASE_URL = 'https://taskma-p59o.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+// const API_BASE_URL = 'https://taskma-p59o.onrender.com';
 
 export { API_BASE_URL };
 
